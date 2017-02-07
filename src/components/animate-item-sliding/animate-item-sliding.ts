5d61b178fa9c22c237fe91187e075b934ac44722
Ted Wei <jdwei@hotmail.com>
2017-02-08 06:57:42 +0800

    ionic g directive AnimateItemSliding

---
+++ src/components/animate-item-sliding/animate-item-sliding.ts
@@ -0,0 +1,18 @@
+import { Directive } from '@angular/core';
+
+/*
+  Generated class for the AnimateItemSliding directive.
+
+  See https://angular.io/docs/ts/latest/api/core/index/DirectiveMetadata-class.html
+  for more info on Angular 2 Directives.
+*/
+@Directive({
+  selector: '[animate-item-sliding]' // Attribute selector
+})
+export class AnimateItemSliding {
+
+  constructor() {
+    console.log('Hello AnimateItemSliding Directive');
+  }
+
+}
